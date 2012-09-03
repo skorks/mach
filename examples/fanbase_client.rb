7775d7356e4378ae9657bdb297c5d530ba4bce90
Dhiraj Bhandari <mailbox.rajbhandari@gmail.com>
2012-09-03 13:52:57 +1000

    added some logging

---
+++ examples/fanbase_client.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+require 'bundler/setup'
+require 'mach'
+require 'base64'
+require 'multi_json'
+
+def get_credentials
+  connection = Faraday.new(:url => "http://localhost:9595") do |c|
+    c.adapter Faraday.default_adapter
+  end
+  credentials_response = connection.post { |req| req.url "/credentials" } # first request is to store the client delta
+  credentials = MultiJson.decode(credentials_response.body)
+end
+
+def make_request(id, secret)
+  #make a request using those credentials
+  connection = Faraday.new(:url => "http://localhost:3001/providers") do |c|
+    c.request :hmac_authentication, id, secret
+    c.adapter Faraday.default_adapter
+  end
+  res = connection.get { |req| req.url '/' }
+  [res.status, res.body]
+end
+
+def make_valid_request
+  credentials = get_credentials
+  make_request(credentials["id"], credentials["secret"])
+end
+
+def make_invalid_request
+  credentials = get_credentials
+  make_request(credentials["id"], "XXX")
+end
+
+p make_valid_request
+#p make_invalid_request
--- lib/mach/validation/signature_validator.rb
+++ lib/mach/validation/signature_validator.rb
@@ -1,4 +1,5 @@
 require 'mach/signature'
+require 'logger'
 
 module Mach
   module Validation
@@ -6,10 +7,19 @@ module Mach
     class SignatureValidator
       class << self
         def valid?(hmac_request)
+          debug_log(sprintf('request with mac_id: %s', hmac_request.mac_id))
+
           if secret = credential_store[hmac_request.mac_id]
+            debug_log(sprintf('request found for mac_id: %s', hmac_request.mac_id))
             data = hmac_request.mac_normalized_request_string
-            Mach::Signature.new(secret, data).matches?(hmac_request.mac_signature)
+            debug_log(sprintf('normalized request string: %s', hmac_request.normalized_request_string))
+            signature = Mach::Signature.new(secret, data)
+            result signature.matches?(hmac_request.mac_signature)
+            debug_log("sigature [generated]: %s signature[request] %s, matches?: %s",
+                      signature.to_s, hmac_request.mac_signature, result))
+            result
           else
+            debug_log(sprintf('no secret found for mac_id: %s', hmac_request.mac_id))
             false
           end  
         end
@@ -18,7 +28,16 @@ module Mach
         def credential_store
           Mach::configuration.credential_store
         end
+
+        def logger
+          @log ||= Logger.new('mach_log.txt').tap { |l| l. debug "Log file created" }
+        end
+
+        def debug_log(msg)
+          log.info msg
+        end
       end
     end
   end
 end
+
